816347d15c2d6a4993ce31a334d2f59b3824edfc
dasno95 <dasno95@gmail.com>
2022-02-13 22:58:35 +0400

    SelenideTest.java added

---
+++ src/test/java/dasno95/lesson_6/SelenideTest.java
@@ -0,0 +1,30 @@
+package dasno95.lesson_6;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.*;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void testIssueSearch() {
+        open("https://github.com/");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#74")).should(visible);
+    }
+}
